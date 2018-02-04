61ed68231090f8d7ab2e116f0eb26f492cbb75a5
ssigg <stephan.sigg@gmail.com>
2018-02-04 12:35:20 +0100

    Improve tests and add coverage

--- src/event/events.controller.spec.ts
+++ src/event/events.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Event } from './event.entity';
 import { EventsService } from './events.service';
-import { EventsController } from './events.controller';
+import { EventsController, EventsAdminController } from './events.controller';
 
 describe('EventsController', () => {
     let eventsService: EventsService;
@@ -13,18 +13,49 @@ describe('EventsController', () => {
 
     it('Fetches all events from events service', async () => {
         let eventMock = new Event();
-        eventMock.name = 'Test Event';
-        spyOn(eventsService, 'findAll').and.returnValue([ eventMock ]);
+        let eventsServiceFindAllSpy = spyOn(eventsService, 'findAll').and.returnValue([ eventMock ]);
         let events = eventsController.findAll();
+        expect(eventsServiceFindAllSpy).toHaveBeenCalledTimes(1);
         expect(await events).toEqual([ eventMock ]);
     });
 
     it('Fetches event with given id from events service', async () => {
         let eventMock = new Event();
-        eventMock.id = 1;
-        eventMock.name = 'Test Event';
         let eventsServiceFindSpy = spyOn(eventsService, 'find').and.returnValue(eventMock);
         let event = eventsController.find({ id: 1 });
+        expect(eventsServiceFindSpy).toHaveBeenCalledWith(1);
         expect(await event).toEqual(eventMock);
+    });
+});
+
+describe('EventsAdminController', () => {
+    let eventsService: EventsService;
+    let eventsAdminController: EventsAdminController;
+
+    beforeEach(() => {
+        eventsService = new EventsService(null);
+        eventsAdminController = new EventsAdminController(eventsService);
+    });
+
+    it('Fetches all events from events service', async () => {
+        let eventMock = new Event();
+        let eventsServiceFindAllSpy = spyOn(eventsService, 'findAll').and.returnValue([ eventMock ]);
+        let events = eventsAdminController.findAll();
+        expect(eventsServiceFindAllSpy).toHaveBeenCalledTimes(1);
+        expect(await events).toEqual([ eventMock ]);
+    });
+
+    it('Fetches event with given id from events service', async () => {
+        let eventMock = new Event();
+        let eventsServiceFindSpy = spyOn(eventsService, 'find').and.returnValue(eventMock);
+        let event = eventsAdminController.find({ id: 1 });
+        expect(eventsServiceFindSpy).toHaveBeenCalledWith(1);
+        expect(await event).toEqual(eventMock);
+    });
+
+    it('Deletes an event using events service', () => {
+        let eventsServiceDeleteSpy = spyOn(eventsService, 'delete');
+        eventsAdminController.delete({ id: 1 });
+        expect(eventsServiceDeleteSpy).toHaveBeenCalledTimes(1);
     });
 });
---
+++ src/event/events.service.spec.ts
@@ -0,0 +1,36 @@
+import { Repository } from "typeorm";
+import { Event } from "./event.entity";
+import { EventsService } from "./events.service";
+
+describe('EventsService', () => {
+    let eventRepository: Repository<Event>;
+    let eventsService: EventsService;
+
+    beforeEach(() => {
+        eventRepository = new Repository<Event>();
+        eventsService = new EventsService(eventRepository);
+    });
+
+    it('Fetches all events from repository', async () => {
+        let eventMock = new Event();
+        let eventRepositoryFindSpy = spyOn(eventRepository, 'find').and.returnValue([eventMock]);
+        let events = eventsService.findAll();
+        expect(eventRepositoryFindSpy).toHaveBeenCalledTimes(1);
+        expect(await events).toEqual([eventMock]);
+    });
+
+    it('Fetches event with given id from repository', async () => {
+        let eventMock = new Event();
+        let eventRepositoryFindOneByIdSpy = spyOn(eventRepository, 'findOneById').and.returnValue(eventMock);
+        let event = eventsService.find(1);
+        expect(eventRepositoryFindOneByIdSpy).toHaveBeenCalledWith(1);
+        expect(await event).toEqual(eventMock);
+    });
+
+    it('Deletes event with given id using repository', () => {
+        let eventMock = new Event();
+        let eventRepositoryDeleteSpy = spyOn(eventRepository, 'delete');
+        let event = eventsService.delete(1);
+        expect(eventRepositoryDeleteSpy).toHaveBeenCalledWith({ id: 1 });
+    });
+});
