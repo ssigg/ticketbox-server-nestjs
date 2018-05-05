887d59cba724683c892c9d4727ac31c16826bb26
ssigg <stephan.sigg@gmail.com>
2018-05-05 17:02:15 +0200

    Enable cookies

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -4,7 +4,8 @@ import { Middleware, NestMiddleware, ExpressMiddleware } from "@nestjs/common";
 export class CorsMiddleware implements NestMiddleware {
     resolve(): ExpressMiddleware {
         return (req, res, next) => {
-            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Origin', req.headers.origin);
+            res.header('Access-Control-Allow-Credentials', true);
             res.header('Access-Control-Allow-Headers', 'Content-Type, Access-Controll-Allow-Headers, Authorization, X-Requested-With');
             res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
             next();
